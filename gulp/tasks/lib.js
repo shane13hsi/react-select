7974d25e7537a9e87ab621184ba8be66144ff1e9
Shane Hsi <shane13hsi@gmail.com>
2015-07-14 15:30:04 +0800

    gulp lib:less

--- gulp/tasks/lib.js
+++ gulp/tasks/lib.js
@@ -2,13 +2,20 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var constants = require('../../constants');
 var del = require('del');
+var less = require('gulp-less');
 
-gulp.task('lib', ['lib:clean'], function() {
-    return gulp.src('src/select/**/*.js')
+gulp.task('lib', ['lib:clean', 'lib:less'], function() {
+    return gulp.src(constants.SRC_DIR + '/select/**/*.js')
         .pipe(babel())
         .pipe(gulp.dest(constants.LIB_DIR));
 });
 
 gulp.task('lib:clean', function(done) {
     del([constants.LIB_DIR], done);
+});
+
+gulp.task('lib:less', function() {
+    return gulp.src(constants.SRC_DIR + '/less/select.less')
+        .pipe(less())
+        .pipe(gulp.dest(constants.LIB_DIR));
 });
